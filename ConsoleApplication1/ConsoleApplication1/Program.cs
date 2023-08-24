c8e09b3d28d2d2c2bb9fd1896a2c9d95448bc727
wippo205 <58757344+wippo205@users.noreply.github.com>
2023-08-24 12:46:28 +0700

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TenToaDo.ToString());
+            ToaDo td = new ToaDo();
+            Console.WriteLine(td.ToString());
+            Console.WriteLine(td.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
---
+++ ConsoleApplication1/ConsoleApplication1/ToaDo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class ToaDo
+    {
+
+
+        private double X, Y;
+        private string TenToaDo;
+        public double x
+        {
+            get { return this.X; }
+            set { this.X = value; }
+        }
+        public double y
+        {
+            get { return this.Y; }
+            set { this.Y = value; }
+        }
+        public string tentoado
+        {
+            get { return this.TenToaDo; }
+            set { this.TenToaDo = value; }
+        }
+        public ToaDo(string TenToaDoToaDo, double XToaDo, double YToaDo)
+        {
+            TenToaDo = TenToaDoToaDo;
+            X = XToaDo;
+            Y = YToaDo;
+        }
+        public ToaDo()
+        {
+            TenToaDo = "Luxubu";
+            X = 5;
+            Y = 15;
+        }
+        
+
+    }
+}
